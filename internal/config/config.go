2e59191eade3837194137d8142b3d2e0e5702d5e
tryoo0607 <tryoo0607@gmail.com>
2025-09-01 01:42:11 +0000

    move : 모델을 별도의 파일로 분리

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,20 +9,6 @@ import (
 	"github.com/spf13/viper"
 )
 
-type Server struct {
-	Env  string `mapstructure:"env" envconfig:"APP_ENV" required:"true" default:""`
-	Port string `envconfig:"PORT" required:"true" default:"8080" mapstructure:"-"`
-}
-
-type Test struct {
-	Name string `mapstructure:"name" `
-}
-
-type DefaultConfig struct {
-	Server Server `mapstructure:"server"`
-	Test   Test   `mapstructure:"test"`
-}
-
 var current DefaultConfig
 
 func Load() (cfg DefaultConfig, err error) {
